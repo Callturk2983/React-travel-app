75dc8d4a8afa7fcf4b26caa1daae17b0fdfebf68
Kevin Taylor <kevintaylor&84@gmail.com>
2023-03-01 15:06:29 -0500

    destination componment completed

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -10,9 +10,13 @@ const Destinations = () => {
   return (
     <div className='max-[1240px] text-center mx-auto py-16 px-4'>
         <h1>All-inclusive Resorts</h1>
-        <p> On the Caribbean's Best Beaches</p>
+        <p className='py-4'> On the Caribbean's Best Beaches</p>
         <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4'>
-
+          <img className='w-full h-full object-cover col-span-2 md:col-span-3 row-span-2' src={borabora} alt="/"/>
+          <img className='w-full h-full object-cover' src={borabora2} alt="/"/>
+          <img className='w-full h-full object-cover' src={maldives} alt="/"/>
+          <img className='w-full h-full object-cover' src={maldives2} alt="/"/>
+          <img className='w-full h-full object-cover' src={keywest} alt="/"/>
         </div>
     </div>
   )
